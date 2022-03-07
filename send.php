b87b63e1ed940efc73d7df089c64eaca33d42fb5
vladimir2000 <33676912+vladimir2000@users.noreply.github.com>
2022-03-07 21:36:11 +0900

    Update send.php

--- send.php
+++ send.php
@@ -18,7 +18,7 @@ if ($_POST['act'] == 'order') {
         $txt .= "<b>".$key."</b> ".$value."%0A";
     };
 
-    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+    $sendToTelegram = fopen("https://api.telegram.org/bot5171394641:AAH3URQknZNSc8cS3cTtJGzP1TrHeCwLexc/sendMessage?chat_id=-1001642035907&parse_mode=html&text={$txt}","r");
 
     if ($sendToTelegram) {
         header('Location: index.html');
